ea22c26d5024f77d19fed8236064a74cfb40773b
Klayver Ximenes <klayverx@gmail.com>
2021-04-23 10:27:15 -0300

    aula 05 - finalização das funcionalidades do player

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,20 +1,22 @@
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
-import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString'
-
 import { GetStaticPaths, GetStaticProps } from 'next'
-import { api } from '../../services/api'
-
-import styles from './episode.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
+import Head from 'next/head'
+
+import { api } from '../../services/api'
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString'
+
+import { usePlayer } from '../../contexts/PlayerContext'
+import styles from './episode.module.scss'
 
 type Episode = {
     id: string;
     title: string;
     thumbnail: string;
-    members: number;
-    duration: string;
+    members: string;
+    duration: number;
     durationAsString: string;
     description: string;
     url: string;
@@ -26,8 +28,14 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+    const { play } = usePlayer();
+
     return (
         <div className={styles.episode}>
+            <Head>
+                <title>{episode.title} | Podcastr</title>
+            </Head>
+
             <div className={styles.thumbnailContainer}>
                 <Link href="/">
                     <button type="button">
@@ -40,7 +48,7 @@ export default function Episode({ episode }: EpisodeProps) {
                     src={episode.thumbnail}
                     objectFit="cover"
                 />
-                <button>
+                <button type="button" onClick={() => play(episode)}>
                     <img src="/play.svg" alt="Tocar episódio" />
                 </button>
             </div>
